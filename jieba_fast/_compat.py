b8c5ea948b0cb844a9ec719a9d9b04140acbff29
xiamin <jsdtxm@live.com>
2025-10-28 09:36:09 +0000

    refactor: update _compat.py

--- jieba_fast/_compat.py
+++ jieba_fast/_compat.py
@@ -1,43 +1,35 @@
 # -*- coding: utf-8 -*-
-import os
 import sys
 
 try:
-    import pkg_resources
-    get_module_res = lambda *res: pkg_resources.resource_stream(__name__,
-                                                                os.path.join(*res))
-except ImportError:
-    get_module_res = lambda *res: open(os.path.normpath(os.path.join(
-                            os.getcwd(), os.path.dirname(__file__), *res)), 'rb')
+    from importlib import resources
 
-PY2 = sys.version_info[0] == 2
+    get_module_res = lambda f: str(resources.files("jieba").joinpath(f))
+except ImportError:
+    import importlib_resources
+
+    get_module_res = lambda f: str(importlib_resources.files("jieba").joinpath(f))
 
 default_encoding = sys.getfilesystemencoding()
 
-if PY2:
-    text_type = unicode
-    string_types = (str, unicode)
 
-    iterkeys = lambda d: d.iterkeys()
-    itervalues = lambda d: d.itervalues()
-    iteritems = lambda d: d.iteritems()
+text_type = str
+string_types = (str,)
+xrange = range
 
-else:
-    text_type = str
-    string_types = (str,)
-    xrange = range
+iterkeys = lambda d: iter(d.keys())
+itervalues = lambda d: iter(d.values())
+iteritems = lambda d: iter(d.items())
 
-    iterkeys = lambda d: iter(d.keys())
-    itervalues = lambda d: iter(d.values())
-    iteritems = lambda d: iter(d.items())
 
 def strdecode(sentence):
     if not isinstance(sentence, text_type):
         try:
-            sentence = sentence.decode('utf-8')
+            sentence = sentence.decode("utf-8")
         except UnicodeDecodeError:
-            sentence = sentence.decode('gbk', 'ignore')
+            sentence = sentence.decode("gbk", "ignore")
     return sentence
+
 
 def resolve_filename(f):
     try:
